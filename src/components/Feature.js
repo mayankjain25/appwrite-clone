322af494ae94f0008293e50af94499dafc9477ea
Mayank Jain <jainmayank2509@gmail.com>
2022-01-17 14:00:23 +0530

    features page done

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -5,11 +5,20 @@ import FeatureItem from './FeatureItem'
 const Feature = () => {
     return (
         <div className='Features'>
-            <h2>Power, Security and Flexibility</h2>
-            <p>Take advantage of the Appwrite products to speed up your development on any platform</p>
+            <div className='features-heading'>
+                <h2>Power, Security and Flexibility</h2>
+                <p>Take advantage of the Appwrite products to speed up your development on any platform</p>
+            </div>
 
             <div className='Features-container'>
-                <FeatureItem icon='fas fa-database' title='Database' description='Store, query and manage access control to your app documents' />
+                <FeatureItem icon='fas fa-database fa-2x' title='Database' description='Upload, download and preview your app and users files and media' />
+                <FeatureItem icon='fas fa-hdd fa-2x' title='Storage' description='Store, query and manage access control to your app documents' />
+                <FeatureItem icon='fas fa-user-friends fa-2x' title='Users' description='Authenticate, confirm and manage your users using multiple signin methods' />
+                <FeatureItem icon='fas fa-location-arrow fa-2x' title='GEO & Localization' description='Detect your users location, locale and fetch GEO related data' />
+                <FeatureItem icon='fas fa-bolt fa-2x' title='Functions' description='Run your backend code in a secure and isolated environment to customize your app' />
+                <FeatureItem icon='fas fa-terminal fa-2x' title='Console' description='Track your backend API usage and manage your project resources from a modern UI' />
+                <FeatureItem icon='fas fa-lock fa-2x' title='Privacy' description='Own your data. Easily setup Appwrite self-hosted solution on your infrastructure' />
+                <FeatureItem icon='fas fa-shield-alt fa-2x' title='Security' description='Built in end to end security for your backend API both in transit and at rest' />
             </div>
 
         </div>
--- src/components/FeatureItem.js
+++ src/components/FeatureItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import "../styles/FeatureItem.css"
 
 const FeatureItem = (props) => {
     return (
-        <div>
-            <i className={props.icon}></i>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
+        <div className='feature-item'>
+            <i style={{color:"#f15783"}}className={props.icon}></i>
+            <h2 style={{color:"#f15783",marginTop:"20px",width:"100%", fontSize:"1rem" }}>{props.title}</h2>
+            <p style={{fontSize:"0.75rem",marginTop:"10px"}}>{props.description}</p>
         </div>
     )
 }
